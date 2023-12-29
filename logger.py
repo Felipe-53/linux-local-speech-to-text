c0a1a5084b1fe44692049ba97f2e8dc327737d8f
Felipe-53 <felipeandresb97@gmail.com>
2023-12-29 11:12:56 -0300

    Update logger.py to use absolute path for log file

--- logger.py
+++ logger.py
@@ -1,7 +1,12 @@
 import logging
+import os
 
-logging.basicConfig(filename='logs.txt', 
-                    level=logging.INFO, 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+log_file = os.path.join(current_dir, 'logs.txt')
+
+logging.basicConfig(filename=log_file,
+                    level=logging.INFO,
                     filemode='a',
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
