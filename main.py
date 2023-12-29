8224f9434d4c6729b25a175bfbbff9c434a0da22
Felipe-53 <felipeandresb97@gmail.com>
2023-12-29 08:05:53 -0300

    Add microphone source detection and logging

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import whisper
 import subprocess
+from logger import logger
 
-def transcribe_audio_from_file(file_path, english = True):
+MIC_SOURCE = "alsa_input.usb"
+
+
+def transcribe_audio_from_file(file_path, english=True):
     model_name = "base.en"
     if not english:
         model_name = "base"
@@ -10,25 +14,33 @@ def transcribe_audio_from_file(file_path, english = True):
     result = model.transcribe(file_path)
 
     return result["text"]
-    
 
 
 if __name__ == "__main__":
     is_english = True
 
-    result = subprocess.run(["pgrep", "pw-record"], capture_output=True, encoding="utf-8")
+    result = subprocess.run(["pgrep", "pw-record"],
+                            capture_output=True, encoding="utf-8")
 
     if result.stdout != "":
         subprocess.run(["pkill", "pw-record"], capture_output=True)
-        
+
     else:
-        subprocess.run(["pw-record", "audio.mp3"], capture_output=True)
+        source_id = None
+        result = subprocess.run(
+            ["pactl", "list", "short", "sources"], capture_output=True, encoding="utf-8")
+        sources = result.stdout.split("\n")
+        for source in sources:
+            if MIC_SOURCE in source:
+                source_id = source.split("\t")[0]
+
+        record_command = ["pw-record", "audio.mp3"]
+        logger.info("Microphone source not found, using default")
+        if source_id != None:
+            logger.info("Found microphone source: " + source_id)
+            record_command = ["pw-record", "--target", source_id, "audio.mp3"]
+
+        subprocess.run(record_command, capture_output=True)
         text = transcribe_audio_from_file("audio.mp3", is_english)
         subprocess.run(["wl-copy", text])
-        subprocess.run(["ydotool", "type", "-d", "0", text])
-        
-
-
-
-
-
+        subprocess.run(["ydotool", "type", "-d", "0", "-H", "0", text])
